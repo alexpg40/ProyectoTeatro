ee8968608c38cad1c1cc7db87a12f55351b92779
Alex Perez <DarkB@AlexPerez.lan>
2020-12-02 19:42:14 +0100

    Falta solo el constructor de copia

--- src/pkg2teatro/Acomodador.java
+++ src/pkg2teatro/Acomodador.java
@@ -12,63 +12,16 @@ package pkg2teatro;
 public class Acomodador extends EmpleadoDeTeatro{
     
     public Acomodador(){
-        
-    }
-
-    public char getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(char categoria) {
-        this.categoria = categoria;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellidos() {
-        return apellidos;
-    }
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
-
-    public String getNif() {
-        return nif;
-    }
-
-    public void setNif(String nif) {
-        this.nif = nif;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
+        super();
     }
     
+    public Acomodador(long id, String nombre, String apellidos, String nif, String direccion, String telefono, char categoria){
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.nif = nif;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.categoria = categoria;
+    }
 }
