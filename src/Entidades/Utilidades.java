3f0a9560cefe73a834681539f4228e960fb70567
alex <alex@alex>
2020-12-30 18:18:48 +0100

    Array con empleados en Utilidades

--- src/Entidades/Utilidades.java
+++ src/Entidades/Utilidades.java
@@ -10,5 +10,16 @@ package Entidades;
  * @author alex
  */
 public class Utilidades {
-    
+
+    static Franquicia Franquicia;
+    //long id String nombre String apellidos String nif String direccion String telefono Franquicia Franquicia
+    public static final Empleado EMPLEADOS[] = {
+        new Empleado(1, "Amelio", "Pascual", "12345678A", "Madrid, Las Rozas", "293 932 238", Franquicia),
+        new Empleado(2, "Jose Juan", "Garcia", "23456789S", "Cantabria, Soto", "123 431 314", Franquicia),
+        new Empleado(3, "Pepe", "Perez", "34567891A", "Alicante", "134 123 764", Franquicia),
+        new Empleado(4, "Aitor", "Mendizabal", "54348943C", "Murcia", "345 501 459", Franquicia),
+        new Empleado(5, "Josito", "Fernandez", "83774591A", "Galicia, Vigo", "123 459 230", Franquicia)
+
+    };
+
 }
