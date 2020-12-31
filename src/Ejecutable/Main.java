798b40c66348097ee91ef491d6c88a3ae8890511
Alex Perez <Alex Perez@DESKTOP-B1TCN21.lan>
2020-12-31 16:56:34 +0100

    Pruebas para comprobar que funciona

--- src/Ejecutable/Main.java
+++ src/Ejecutable/Main.java
@@ -5,9 +5,31 @@
  */
 package Ejecutable;
 
+import Entidades.Empleado;
+import Entidades.EmpleadoDeTeatro;
+import Entidades.GTrabajo;
+import Entidades.EmpleadoDeOficina;
+
 public class Main {
     //main
     public static void main(String[] args) {
 
+        String a = "A";
+        String nif = "12345618C";
+        Empleado e = new Empleado();
+        e.setNombre(a);
+        e.comprobarNombre(a);
+        e.comprobarNif(nif);
+        e.comprobarTelefono("123356789");
+        
+        GTrabajo g = new GTrabajo();
+        g.comprobarSemana(5);
+        g.comprobarId(8);
+        
+        EmpleadoDeTeatro et = new EmpleadoDeTeatro();
+        et.comprobarCategoria('D');
+        
+        EmpleadoDeOficina ef = new EmpleadoDeOficina();
+        ef.comprobarAñosplantilla(0);
     }
 }
