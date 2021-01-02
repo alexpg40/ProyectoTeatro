22d8ac4e802a2e709e50fdee07cdc018af311977
Alex Perez <Alex Perez@DESKTOP-B1TCN21.lan>
2021-01-02 15:57:33 +0100

    Arreglado el metodo nuevo empleado :D

--- src/Entidades/Empleado.java
+++ src/Entidades/Empleado.java
@@ -101,110 +101,187 @@ public class Empleado {
     }
 
     public void comprobarId(long id) {
-        if (id <= 0) {
-            System.out.println("La Id tiene que ser mayor estrictamente que 0");
-            return;
-        } else if (id % 1 != 0) {
-            System.out.println("La id tiene que ser un número entero");
-            return;
+        boolean cond = false;
+        if (id < 0 || id == 0) {
+            System.out.println("La id tiene que mayor estrictamente que cero");
         } else {
+            cond = false;
             for (Empleado Empleado : Utilidades.EMPLEADOS) {
                 if (id == Empleado.getId()) {
-                    System.out.println("No se puede repetir la id");
-                    return;
+                    cond = true;
+                    break;
                 } else {
-                    return;
+                    cond = false;
                 }
+            }
+            if (cond) {
+                System.out.println("La id coincide con alguna ya guardada en la base");
+            } else {
+                this.id = id;
             }
         }
     }
 
     public void comprobarNombre(String nombre) {
+        boolean cond = false;
         if (nombre.isEmpty()) {
             System.out.println("No puede estar vacio");
         } else {
             for (int i = 0; i < nombre.length(); i++) {
-                if ((nombre.charAt(i) >= 'a' && nombre.charAt(i) <= 'z') || (nombre.charAt(i) >= 'A' && nombre.charAt(i) <= 'Z')) {
+                if (((nombre.charAt(i) >= '0' && nombre.charAt(i) <= '9'))) {
+                    cond = true;
+                    break;
                 } else {
-                    System.out.println("Solo se permiten letras");
-                    return;
+                    cond = false;
                 }
             }
+
+        }
+        if (cond) {
+            System.out.println("No puede tener números");
+        } else {
+            this.nombre = nombre;
         }
     }
-    
-    public void comprobarApellidos(String apellidos){
+
+    public void comprobarApellidos(String apellidos) {
+        boolean cond = false;
         if (apellidos.isEmpty()) {
             System.out.println("No puede estar vacio");
+            return;
         } else {
             for (int i = 0; i < apellidos.length(); i++) {
-                if ((apellidos.charAt(i) >= 'a' && apellidos.charAt(i) <= 'z') || (apellidos.charAt(i) >= 'A' && apellidos.charAt(i) <= 'Z')) {
+                if (((apellidos.charAt(i) >= '0' && apellidos.charAt(i) <= '9'))) {
+                    cond = true;
+                    break;
                 } else {
-                    System.out.println("Solo se permiten letras");
-                    return; 
+                    cond = false;
                 }
             }
+
+        }
+        if (cond) {
+            System.out.println("No puede tener números");
+        } else {
+            this.apellidos = apellidos;
         }
     }
-    
-    public void comprobarNif(String nif){
+
+    public void comprobarNif(String nif) {
+        boolean cond = false;
         if (nif.length() != 9) {
             System.out.println("El nif debeser de 9 digitos");
-        } else{
-            if (!((nif.charAt(8) >= 'a' && nif.charAt(8) <= 'z' ) || (nif.charAt(8) >= 'A' && nif.charAt(8) <= 'Z'))) {
-                System.out.println("Debe finalizar con una letra");
-            } else{
-                for (int i = 0; i < 7; i++) {
-                    if (!(nif.charAt(i) >= '0' && nif.charAt(i) <= '9')) {
-                        System.out.println("Los primeros 8 digitos deben ser numeros");
+            return;
+        } else if (!((nif.charAt(8) >= 'a' && nif.charAt(8) <= 'z') || (nif.charAt(8) >= 'A' && nif.charAt(8) <= 'Z'))) {
+            System.out.println("Debe finalizar con una letra");
+        } else {
+            for (int i = 0; i < 7; i++) {
+                if (!(nif.charAt(i) >= '0' && nif.charAt(i) <= '9')) {
+                    System.out.println("Los primeros 8 digitos deben ser numeros");
+                } else {
+                    for (Empleado e : Utilidades.EMPLEADOS) {
+                        if (e.getNif().equals(nif)) {
+                            cond = true;
+                            break;
+                        } else {
+                            cond = false;
+                        }
                     }
                 }
             }
         }
-    }
-    
-    public void comprobarDireccion(String direccion){
-        if (direccion.isEmpty()) {
-            System.out.println("La direccion no puede estar vacia");
+        if (cond) {
+            System.out.println("Este nif ya esta registrado en la base");
+        } else {
+            this.nif = nif;
         }
     }
-    
-    public void comprobarTelefono(String telefono){
-        if (telefono.length() != 9) {
-            System.out.println("Debe tener nueve numeros");
-        } else{
-            for (int i = 0; i < telefono.length(); i++) {
-                if (!(telefono.charAt(i) >= '0' && telefono.charAt(i) <='9')) {
-                    System.out.println("Solo se permiten números");
+
+    public void comprobarDireccion(String direccion) {
+        if (direccion.isEmpty()) {
+            System.out.println("La direccion no puede estar vacia");
+        } else {
+            this.direccion = direccion;
+        }
+    }
+
+    public void comprobarTelefono(String telefono) {
+        boolean cond = false;
+        if (telefono.contains(CharSequence.class.cast(" "))) {
+            System.out.println("No puede haber espacios");
+            return;
+        } else {
+            if (telefono.length() != 9) {
+                System.out.println("Debe tener nueve numeros");
+                return;
+            } else {
+                for (int i = 0; i < telefono.length(); i++) {
+                    if (!(telefono.charAt(i) >= '0' && telefono.charAt(i) <= '9')) {
+                        cond = true;
+                        break;
+                    } else {
+                        cond = false;
+                    }
                 }
+            }
+
+            if (cond) {
+                System.out.println("Solo se permiten números");
+            } else {
+                this.telefono = telefono;
             }
         }
     }
-    
-    public Empleado nuevoEmpleado(){
+
+    public Empleado nuevoEmpleado() {
         Empleado e = new Empleado();
         Scanner in = new Scanner(System.in);
         System.out.println("Introduce el id");
-        long co = in.nextLong();
-        e.comprobarId(co);
-        e.setId(co);
+        long co;
+        do {
+            co = -1;
+            in = new Scanner(System.in);
+            co = in.nextLong();
+            e.comprobarId(co);
+        } while (e.getId() != co || co == 0);
         System.out.println("Introduce el nombre");
-        String us = in.nextLine();
-        e.comprobarNombre(us);
-        e.setNombre(us);
+        String us;
+        do {
+            in = new Scanner(System.in);
+            us = in.nextLine();
+            e.comprobarNombre(us);
+        } while (e.getNombre() != us);
         System.out.println("Introduce los apellidos");
-        String ap = in.nextLine();
-        e.comprobarApellidos(ap);
-        e.setApellidos(ap);
+        String ap;
+        do {
+            in = new Scanner(System.in);
+            ap = in.nextLine();
+            e.comprobarApellidos(ap);
+        } while (e.getApellidos() != ap);
         System.out.println("Introduce el NIF");
-        String n = in.nextLine();
-        e.comprobarNif(n);
-        e.setNif(n);
+        String n;
+        do {
+            in = new Scanner(System.in);
+            n = in.nextLine();
+            e.comprobarNif(n);
+        } while (e.getNif() != n);
         System.out.println("Introduce el telefono");
-        String t = in.nextLine();
-        e.comprobarTelefono(t);
-        e.setTelefono(t);
-    return e;}
+        String t;
+        do {
+            in = new Scanner(System.in);
+            t = in.nextLine();
+            e.comprobarTelefono(t);
+        } while (e.getTelefono() != t);
+        System.out.println("Introduce tu direccion");
+        String d;
+        do {
+            in = new Scanner(System.in);
+            d = in.nextLine();
+            e.comprobarDireccion(d);
+        } while (e.getDireccion() != d);
+
+        return e;
+    }
 
     @Override
     public String toString() {
