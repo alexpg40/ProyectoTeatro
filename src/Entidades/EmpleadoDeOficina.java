6d35d02050d0c9701b03144a2bd5dddb2d11e7b5
DarkB <DarkB@LAPTOP-FDCHG8J4>
2021-03-11 19:21:32 +0100

    validacion de añosenplantilla de empleado de oficina

--- src/Entidades/EmpleadoDeOficina.java
+++ src/Entidades/EmpleadoDeOficina.java
@@ -6,30 +6,32 @@
 package Entidades;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
  *
  * @author Sandr
  */
-public class EmpleadoDeOficina extends Empleado{
-    
+public class EmpleadoDeOficina extends Empleado {
+
     //VALORES VALIDOS: ENTERO => 0 Y ENTERO <= 80 && VALORES INVALIDOS: ENTEROS > 80 o < 0, CUALQUIER CARACTER DIFERENTE A UN NUMERO 
-   protected int añosPlantilla;
-    
-    
-    public EmpleadoDeOficina () {
+    protected int añosPlantilla;
+
+    public EmpleadoDeOficina() {
         super();
     }
-    public EmpleadoDeOficina(Empleado e){
+
+    public EmpleadoDeOficina(Empleado e) {
         super(e);
     }
-    public EmpleadoDeOficina (Empleado e, int añosPlantilla) {
+
+    public EmpleadoDeOficina(Empleado e, int añosPlantilla) {
         super(e);
         this.añosPlantilla = añosPlantilla;
-    } 
-    
-    public EmpleadoDeOficina(long id, String nombre, String apellidos, String nif, String direccion, String telefono, char categoria){
+    }
+
+    public EmpleadoDeOficina(long id, String nombre, String apellidos, String nif, String direccion, String telefono, char categoria) {
         super.id = id;
         super.nombre = nombre;
         super.apellidos = apellidos;
@@ -45,51 +47,58 @@ public class EmpleadoDeOficina extends Empleado{
     public void setAñosPlantilla(int añosPlantilla) {
         this.añosPlantilla = añosPlantilla;
     }
-    
+
     /**
      * Valida los años en plantilla
+     *
      * @param añosPlantilla a validar
      * @return boolean
      */
-    public boolean validarAñosplantilla(int añosPlantilla){
+    public boolean validarAñosplantilla(int añosPlantilla) {
         //Valida que los años sean entre 0 como minimo y 80 como máximo.
-        if (!(añosPlantilla >= 0 && añosPlantilla <80)) {
+        if (!(añosPlantilla >= 0 && añosPlantilla < 80)) {
             System.out.println("Los años en plantilla tiene que estar entre 0 y 80");
             //Setea los años en plantilla del empleado.
-        } else{
+        } else {
             return true;
         }
-    return true;
+        return true;
     }
-    
+
     /**
      * Crea y devuelve un empleado de oficina con los datos introducidos.
-     * @param empleados 
-     * @return 
+     *
+     * @param empleados
+     * @return
      */
-    public static EmpleadoDeOficina nuevoEmpleadoDeOficina(ArrayList<Empleado> empleados){
+    public static EmpleadoDeOficina nuevoEmpleadoDeOficina(ArrayList<Empleado> empleados) {
         Scanner sc;
         Empleado e = Empleado.nuevoEmpleado(empleados);
         EmpleadoDeOficina ef = new EmpleadoDeOficina(e);
-        System.out.println("Introcude los años en plantilla");
-        int a;
+        System.out.println("Introdude los años en plantilla");
+        int a = 1;
         do {
             sc = new Scanner(System.in);
-            a = sc.nextInt();
+            try {
+                a = sc.nextInt();
+            } catch (InputMismatchException ex) {
+                System.out.println("Solo se permiten números enteros");
+            }
             if (ef.validarAñosplantilla(a)) {
                 ef.setAñosPlantilla(a);
             }
         } while (ef.getAñosPlantilla() != a);
-    return ef;}
-    
+        return ef;
+    }
+
     @Override
     public String toString() {
-        return "Empleado{" + "id=" + id + ", nombre=" + nombre + ", apellidos=" + apellidos + ", nif=" + nif + ", direccion=" + direccion + ", telefono=" + telefono + ", años en plantilla" + añosPlantilla +'}';
+        return "Empleado{" + "id=" + id + ", nombre=" + nombre + ", apellidos=" + apellidos + ", nif=" + nif + ", direccion=" + direccion + ", telefono=" + telefono + ", años en plantilla" + añosPlantilla + '}';
     }
-    
-   @Override
+
+    @Override
     public String data() {
         return this.id + "|" + this.nombre + "|" + this.apellidos + "|" + this.nif + "|" + this.direccion + "|" + this.telefono + "|" + this.añosPlantilla;
     }
-    
+
 }
