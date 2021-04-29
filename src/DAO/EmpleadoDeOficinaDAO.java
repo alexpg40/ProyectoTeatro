7b3362b966b50a336c6bb94306985e09bf6a4800
DarkB <DarkB@LAPTOP-FDCHG8J4>
2021-04-29 19:45:46 +0200

    terminado :D!

--- src/DAO/EmpleadoDeOficinaDAO.java
+++ src/DAO/EmpleadoDeOficinaDAO.java
@@ -19,7 +19,7 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author DarkB
+ * @author Alex
  */
 public class EmpleadoDeOficinaDAO {
 
--- src/DAO/EmpleadoDeTeatroDAO.java
+++ src/DAO/EmpleadoDeTeatroDAO.java
@@ -18,7 +18,7 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author DarkB
+ * @author Alex
  */
 public class EmpleadoDeTeatroDAO {
     
