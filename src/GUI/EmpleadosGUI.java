384b99977074c12b076bb297bb558899f5dd2ac6
DarkB <DarkB@LAPTOP-FDCHG8J4>
2021-05-15 20:38:15 +0200

    evento para llevar al jFrame para crear el empleado

--- src/GUI/EmpleadosGUI.java
+++ src/GUI/EmpleadosGUI.java
@@ -134,7 +134,10 @@ public class EmpleadosGUI extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        this.setVisible(false);
+        nuevoEmpleado ne = new nuevoEmpleado();
+        ne.setLocationRelativeTo(this);
+        ne.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
